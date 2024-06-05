0baac54c14ee9d81650cf4e7e13a68e120124e96
Samat168 <supersamat2004@gmail.com>
2024-06-05 14:19:35 +0600

    initial commit

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -2,4 +2,4 @@ export const LOGIN_ROUTE = "/login";
 export const CHAT_ROUTE = "/chat";
 export const ADMIN_ROUTE = "/admin";
 
-export const ADMIN = "supersamat2004@gmail.com";
+export const ADMIN = "admin@gmail.com";
